232df3ddb42d366f1ebf3f4f20cf300df04bf853
Matthino868 <matthino868@gmail.com>
2024-02-08 23:03:03 +0100

    Added readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LivekitAccestokenFlutter
- Creates an livekit accestoken for fllutter applications
+# Dart LiveKit Access Token Generator
+This Dart code demonstrates how to generate LiveKit access tokens using JSON Web Tokens (JWT) for authentication. 
+
+**Prerequisites**
+* Dart SDK installed on your machine.
+* Basic understanding of Dart programming language.
+* Import the JSON Web Token package
--- createToken.dart
+++ createToken.dart
@@ -2,14 +2,15 @@ import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 
 String createToken(){
   // Minutes to live
-  int ttl = 2;
+  final int ttl = 2;
   final String API_KEY = '<API_KEY>';
   final String SECRET_KEY = '<SECRET_KEY>';
   final String room_name = '<name_of_room>';
+  final String nickname = '<your_name>';
+
   final opts = JWT({
     'iss': API_KEY,
-    // Nickname in room
-    'sub': 'CompanionApp',
+    'sub': nickname,
     'iat': DateTime.now().millisecondsSinceEpoch,
     'exp': DateTime.now().millisecondsSinceEpoch + ttl*1000,
     'video': {
